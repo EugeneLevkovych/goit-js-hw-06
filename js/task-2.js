655e35304c4ce45d1917a31b9ffcd33a050fe02e
EugeneLevkovych <eugenelevkovych@yahoo.com>
2024-12-01 18:24:42 +0200

    second attempt

--- js/task-2.js
+++ js/task-2.js
@@ -1,18 +1,22 @@
 class Storage {
-  constructor(items) {
-    this.items = items;
+  #items;
+  constructor(initialItems) {
+    this.#items = initialItems;
   }
   getItems() {
-    return this.items;
+    return this.#items;
   }
   addItem(newItem) {
-    this.items.push(newItem);
+    this.#items.push(newItem);
   }
+  // removeItem(itemToRemove) {
+  //   const index = this.#items.indexOf(itemToRemove);
+  //   if (index !== -1) {
+  //     this.#items.splice(index, 1);
+  //   }
+  // }
   removeItem(itemToRemove) {
-    const index = this.items.indexOf(itemToRemove);
-    if (index !== -1) {
-      this.items.splice(index, 1);
-    }
+    this.#items = this.#items.filter(item => item !== itemToRemove);
   }
 }
 const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
